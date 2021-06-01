4db344d7f335f021dab023dc7c6e2e5a462c1fe3
Ahmed Lahrizi <ahmed.lahrizi05@gmail.com>
2021-06-01 15:02:03 +0000

    added separate config files for build & prod !!

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
 const path = require('path');
 
-module.exports = {
+module.exports = merge (common, {
     mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-}
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
 const path = require('path');
 
-module.exports = {
+module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-}
+});
